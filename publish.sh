d4671b69aaac607416a59eeb9b6792e72c25c20d
Christopher R. Miller <xpm@mysteriouspants.com>
2016-03-04 23:14:47 -0800

    Update deploy script.
    Finally have resolved to stop logging into my server as root all the time.

--- publish.sh
+++ publish.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-rsync -avz _site/ root@mysteriouspants.com:/var/www/mysteriouspants.com/
+rsync -avz _site/ xpm@mysteriouspants.com:/var/www/mysteriouspants.com/
+ssh xpm@mysteriouspants.com chmod -R ugo+rx /var/www/mysteriouspants.com
