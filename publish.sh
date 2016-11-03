d2e9daaff19207a26a487594530cd4df9ebec7e9
Christopher R. Miller <xpm@mysteriouspants.com>
2016-11-03 00:04:45 -0700

    Fix permissions issue in publish script.

--- publish.sh
+++ publish.sh
@@ -6,5 +6,7 @@ REMOTE_PATH=/var/www/www.mysteriouspants.com
 
 bundle exec jekyll build
 
+ssh ${REMOTE_USER}@${REMOTE_HOST} sudo chown xpm:xpm ${REMOTE_PATH}
 rsync -avz _site/ ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH}/
-ssh ${REMOTE_USER}@${REMOTE_HOST} chmod -R 644 ${REMOTE_PATH}
+ssh ${REMOTE_USER}@${REMOTE_HOST} "find ${REMOTE_PATH} -type d -exec chmod 755 {} \;"
+ssh ${REMOTE_USER}@${REMOTE_HOST} "find ${REMOTE_PATH} -type f -exec chmod 644 {} \;"
